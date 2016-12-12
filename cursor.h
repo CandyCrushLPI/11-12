d701af66cbf6925f1220bf0423d202e2ad541169
CandyCrushLPI <CandyCrushLPI@gmail.com>
2016-12-12 01:59:07 -0200

    Update cursor.h

--- cursor.h
+++ cursor.h
@@ -1,6 +1,13 @@
 #ifndef CURSOR_H_INCLUDED
 #define CURSOR_H_INCLUDED
-
-
+typedef struct
+{
+    char nome[25];
+    int pontos;
+} ranking;
+void mostrar();
+void abre_arquivo(void);
+void inserir();
+int dados(int i);
 int cursor(int x, int y);
 #endif // CURSOR_H_INCLUDED
